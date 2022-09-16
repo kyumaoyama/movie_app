b0d3becdb201c49baf2409fb2ae8a63eafbeadfc
kyumaoyama <110519486+kyumaoyama@users.noreply.github.com>
2022-09-16 13:12:35 +0900

    映画投稿ページ遷移成功

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+
+  belongs_to :user
+
+  belongs_to :category
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :rebyu
+    validates :category_id
+  end
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    
+  end
 end
