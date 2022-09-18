9ab12533db97fb2b861d61d6910fa58d90facbba
kyumaoyama <110519486+kyumaoyama@users.noreply.github.com>
2022-09-18 14:33:42 +0900

    コメント投稿、表示作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,16 @@
 class CommentsController < ApplicationController
 
   def create
-    Comment.create(comment_params)
-    redirect_to "/posts/#{comment.post.id}"
+    @comment = current_user.comments.new(comment_params)
+    if @comment.save
+      redirect_to root_path  
+    else
+      redirect_back(fallback_location: root_path)  
+    end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
